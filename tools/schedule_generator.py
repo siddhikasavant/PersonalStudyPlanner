2b6853d37372e8ac3ecd47d7eea537d52be1aec0
Siddhika Savant <siddhikasavant00@gmail.com>
2025-11-29 20:20:41 +0530

    Update schedule_generator.py

--- tools/schedule_generator.py
+++ tools/schedule_generator.py
@@ -1,1 +1,17 @@
+from datetime import datetime, timedelta
+
+def generate_schedule(subjects, start_date, study_hours_per_day=2):
+    schedule = []
+    current_date = datetime.strptime(start_date, "%Y-%m-%d")
+    
+    for i, subject in enumerate(subjects):
+        session = {
+            "subject": subject,
+            "date": current_date.strftime("%Y-%m-%d"),
+            "duration": study_hours_per_day
+        }
+        schedule.append(session)
+        current_date += timedelta(days=1)
+    
+    return schedule
 
